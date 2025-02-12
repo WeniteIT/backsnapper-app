5e72d8cf0f48378ee32834576e9069d815be757e
tFiedlerWenite <t.fiedler@wenite.de>
2025-02-12 16:42:37 +0100

    s

--- app/_components/BaseSection.tsx
+++ app/_components/BaseSection.tsx
@@ -7,7 +7,7 @@ interface IProps extends PropsWithChildren {
 
 export default function BaseSection({ children, label, info }: IProps) {
   return (
-    <div className="flex flex-col gap-4 secondary-light rounded-lg p-4 border-3 secondary-border">
+    <div className="flex flex-col gap-4 secondary-light rounded-lg p-4 md:shadow-lg">
       <div className="flex justify-between items-center">
         <div className="flex text-xl md:text-3xl font-bold">{label}</div>
         <div className="flex text-xl md:text-3xl secondary-text-light">
