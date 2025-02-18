fa20e8ae73d439b08530b9be64c6a034013beced
tFiedlerWenite <t.fiedler@wenite.de>
2025-02-18 17:29:25 +0100

    asdasd

--- app/_components/StatisticCard.tsx
+++ app/_components/StatisticCard.tsx
@@ -4,13 +4,12 @@ import { IResult } from "../interfaces";
 import { _AnimatedNumber } from "./_AnimatedNumbers";
 
 interface IProps {
-  icon?: React.ReactNode;
   label: string ;
   content?: React.ReactNode;
   result?: IResult;
 }
 
-export default function StatisticCard({ icon, result, label, content }: IProps) {
+export default function StatisticCard({  result, label, content }: IProps) {
   return (
     <div
       title={result?.name.join(",")}
